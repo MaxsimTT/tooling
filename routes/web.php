6b15099185b1837191b403d4133aba1b9d741738
MaxsimTT <egorovcotmax@gmail.com>
2022-12-21 09:25:52 +0400

    Revert "added pages auth"
    This reverts commit 2d562ce523ff4e219a2788f876c52c0d6fe78174.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomePageController;
 use App\Http\Controllers\DownloadFileController;
@@ -23,15 +22,3 @@ use App\Http\Controllers\DownloadFileController;
 Route::get('/', [HomePageController::class, 'getHomePage'])->name('homePage');
 Route::post('/download', [DownloadFileController::class, 'setTool'])->name('downloadFile');
 Route::get('/image_delete/{image_id}', [DownloadFileController::class, 'deleteImage'])->name('deleteFile');
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-require __DIR__.'/auth.php';
